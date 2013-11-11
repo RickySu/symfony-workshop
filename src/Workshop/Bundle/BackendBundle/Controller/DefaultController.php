23373b999f36c0f8cfcbf41fa4e28afc3e00a7de
Ricky Su <ricky@ez2.us>
2013-11-11 12:34:39 +0800

    add Custom Login theme

--- src/Workshop/Bundle/BackendBundle/Controller/DefaultController.php
+++ src/Workshop/Bundle/BackendBundle/Controller/DefaultController.php
@@ -4,8 +4,8 @@ namespace Workshop\Bundle\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
@@ -14,8 +14,25 @@ class DefaultController extends Controller
      * @Template()
      */
     public function indexAction()
-    {        
+    {
         return array();
     }
-       
+
+    /**
+     * @Route("/login", name="@BackendLogin")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $csrfToken = $this->get('form.csrf_provider')->generateCsrfToken('authenticate');
+        $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        return array(
+            'csrfToken' => $csrfToken,
+            'lastUsername' => $session->get(SecurityContext::LAST_USERNAME),
+            'error' => ($error?$error->getMessage():null),
+        );
+    }
+
 }
